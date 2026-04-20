409cf02f9fa7363d00a70a1f64275ed9a0383e8c
Tuan M. Trinh <100523037+tuanmtrinh@users.noreply.github.com>
2026-04-20 17:35:51 +0700

    Update schedule.js

--- utils/schedule.js
+++ utils/schedule.js
@@ -10,50 +10,51 @@ export function getSchedules(now = new Date()) {
     return `${d.getFullYear()}-${pad(d.getMonth()+1)}-${pad(d.getDate())} ${pad(d.getHours())}:00`;
   };
 
-// 08:00
-if (hour === 8) {
-  schedules.push({
-    label: "08_OPM24",
-    from: shift(24),
-    to: now
-  });
-
-  schedules.push({
-    label: "08_NS12",
-    from: shift(12),
-    to: now
-  });
-}
-
-// 14:00
-if (hour === 14) {
-  schedules.push({
-    label: "14_TPM24",
-    from: shift(24),
-    to: now
-  });
-
-  schedules.push({
-    label: "14_DAY6",
-    from: shift(6),
-    to: now
-  });
-}
-
-// 20:00
-if (hour === 20) {
-  schedules.push({
-    label: "20_DAY12",
-    from: shift(12),
-    to: now
-  });
-}
-
-// 02:00
-if (hour === 2) {
-  schedules.push({
-    label: "02_NIGHT8",
-    from: shift(8),
-    to: now
-  });
+  // 08:00
+  if (hour === 8) {
+    schedules.push({
+      label: "08_OPM24",
+      from: shift(24),
+      to: now
+    });
+  
+    schedules.push({
+      label: "08_NS12",
+      from: shift(12),
+      to: now
+    });
+  }
+  
+  // 14:00
+  if (hour === 14) {
+    schedules.push({
+      label: "14_TPM24",
+      from: shift(24),
+      to: now
+    });
+  
+    schedules.push({
+      label: "14_DAY6",
+      from: shift(6),
+      to: now
+    });
+  }
+  
+  // 20:00
+  if (hour === 20) {
+    schedules.push({
+      label: "20_DAY12",
+      from: shift(12),
+      to: now
+    });
+  }
+  
+  // 02:00
+  if (hour === 2) {
+    schedules.push({
+      label: "02_NIGHT8",
+      from: shift(8),
+      to: now
+    });
+  }
 }
